4a96805106d01624b41d4433aafb95f608a1d1a4
houstonhaynes <8174976+houstonhaynes@users.noreply.github.com>
2026-09-04 09:17:37 -0400

    chore: sweep the FNCS-era test corpus, plans, upstream docs and root artefacts; retire the naming family in the gate

--- tests/FSharp.Native.Compiler.Service.Tests/CompilerTestHelpers.fs
+++
@@ -1,9 +0,0 @@
-﻿namespace FSharp.Native.Compiler.Service.Tests
-
-[<AutoOpen>]
-module CompilerTestHelpers =
-
-    let (|Warning|_|) (exn: System.Exception) =
-        match exn with
-        | :? FSharp.Native.Compiler.DiagnosticsLogger.DiagnosticWithText as e -> Some (e.number, e.message)
-        | _ -> None
--- tests/benchmarks/FNCSBenchmarks/CompilerServiceBenchmarks/Program.fs
+++
@@ -1,9 +0,0 @@
-﻿open BenchmarkDotNet.Running
-open FSharp.Native.Compiler.Benchmarks
-open BenchmarkDotNet.Configs
-
-[<EntryPoint>]
-let main args =
-    let cfg = ManualConfig.Create(DefaultConfig.Instance).WithOptions(ConfigOptions.DisableOptimizationsValidator)
-    BenchmarkSwitcher.FromAssembly(typeof<DecentlySizedStandAloneFileBenchmark>.Assembly).Run(args,cfg) |> ignore
-    0
